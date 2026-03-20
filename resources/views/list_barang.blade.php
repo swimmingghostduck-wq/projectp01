174423999af891c3b36cb111633dee8b2906071b
swimmingghostduck-wq <swimmingghostduck@gmail.com>
2026-03-20 12:59:11 +0700

    p03

---
+++ resources/views/list_barang.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <h1>List Barang</h1>
+    <p>ID: {{ $id }}</p>
+    <p>Nama: {{ $nama }}</p>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,30 @@ use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/contact', [HomeController::class, 'contact']);
+
+Route::get('/welcome', function () {
+    return "Welcome";
+});
+
+Route::get('/user/{id}', function ($id) {
+    return "User ID: " . $id;
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return "Admin Dashboard";
+    });
+
+    Route::get('/users', function () {
+        return "Admin Users";
+    });
+});
+
+Route::get('/listbarang/{id}/{nama}', function ($id, $nama) {
+    return view('list_barang', compact('id', 'nama'));
+});
+
+use App\Http\Controllers\ListBarangController;
+
+Route::get('/listbarang/{id}/{nama}', [ListBarangController::class, 'show']);
 ?>
